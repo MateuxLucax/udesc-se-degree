6f3750758387a6feec7dda0d28f0009cd5dd715c
Mateus Lucas <mateuxlucax@gmail.com>
2022-08-22 19:14:05 -0300

    refac: move first app to folder
    ooopsie, I made a mistake making firstApp outside of its folder

--- 65-dsd/sockets-intro/src/main/java/Server.java
+++ 65-dsd/sockets-intro/src/main/java/Server.java
@@ -1,5 +1,4 @@
 import java.io.BufferedReader;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
@@ -14,27 +13,11 @@ public class Server {
             while (true) {
                 System.out.println("Awaiting connection...");
                 try (Socket connection = server.accept()) {
-                    connection.
                     System.out.println("Connected with " + connection.getInetAddress().getHostAddress());
 
                     OutputStream response = connection.getOutputStream();
                     String message = "Hello world!";
                     response.write(message.getBytes());
-
-                    BufferedReader in =
-                            new BufferedReader(
-                                    new InputStreamReader(connection.getInputStream()));
-                    String fromServer = in.readLine();
-                    System.out.println(fromServer);
-//                    InputStream response2 = connection.getInputStream();
-//
-//                    byte[] rawData = new byte[1024];
-//                    int readBytes = response2.read(rawData);
-//                    while (readBytes >= 0) {
-//                        String data = new String(rawData, 0, readBytes);
-//                        System.out.println(data);
-//                        readBytes = response2.read(rawData);
-//                    }
                 }
             }
         } catch (Exception e) {
