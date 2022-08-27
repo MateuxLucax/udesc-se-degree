44fe2b126bb9e0b40e5ad82cab8b85635a23f713
MateuxLucax <mateuxlucax@gmail.com>
2022-08-26 23:13:34 -0300

    fix: cli socket server

--- 65-dsd/sockets-intro/src/main/java/cli/Client.java
+++ 65-dsd/sockets-intro/src/main/java/cli/Client.java
@@ -2,35 +2,41 @@ package cli;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) {
-        while (true) {
-            System.out.println("Creating connection...");
-            try (Socket connection = new Socket("127.0.0.1", 80)) {
-                System.out.println("Connected!");
-                DataOutputStream request = new DataOutputStream(connection.getOutputStream());
-                BufferedReader response = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        System.out.println("Creating connection...");
+        try (Socket connection = new Socket("127.0.0.1", 80)) {
+            System.out.println("Connected!");
+
+            try (
+                DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
                 BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
-
-                String text;
-                while ((text = response.readLine()) != null) {
-                    System.out.println(text);
-                    if (text.equals("Connection closed")) break;
-                    String body = keyboard.readLine();
-                    request.writeBytes(body + "\n");
-                }
-
-                request.close();
-                response.close();
-                keyboard.close();
-            } catch (Exception e) {
-                System.out.println("Error while connection to host: " + e.getMessage());
-                break;
+            ) {
+                process(out, in, keyboard);
             }
+        } catch (Exception e) {
+            System.out.println("Error while connection to host: " + e.getMessage());
         }
     }
+
+    public static void process(DataOutputStream out, BufferedReader in, BufferedReader keyboard) throws IOException {
+        StringBuilder response = new StringBuilder();
+        for (String line; (line = in.readLine()) != null;) {
+            if (line.equals(Server.FINISH_MESSAGE)) break;
+            System.out.println(line);
+            response.append(line);
+        }
+
+        if (response.toString().equals("Connection closed")) return;
+        String body = keyboard.readLine();
+        out.writeBytes(body + "\n");
+        process(out, in, keyboard);
+    }
+
 }
 
--- 65-dsd/sockets-intro/src/main/java/cli/Server.java
+++ 65-dsd/sockets-intro/src/main/java/cli/Server.java
@@ -1,13 +1,17 @@
 package cli;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Server {
+
+    public static String FINISH_MESSAGE = "--finish--";
+    public static String CLOSED_MESSAGE = "Connection closed";
+
     public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(80)) {
             server.setReuseAddress(true);
@@ -18,6 +22,7 @@ public class Server {
                     handleConnection(connection);
                 } catch (Exception e) {
                     System.out.println("Error while handling connection. " + e.getMessage());
+                    break;
                 }
             }
         } catch (Exception e) {
@@ -26,51 +31,50 @@ public class Server {
     }
 
     public static void handleConnection(Socket connection) throws IOException {
-        PrintStream response = new PrintStream(connection.getOutputStream());
-        response.flush();
-        System.out.println("Connected with " + connection.getInetAddress().getHostAddress());
-        response.printf("Connected on %s. Please type a CLI command for this OS, or exit to close connection.%n", System.getProperty("os.name"));
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String command;
-        while ((command = reader.readLine()) != null) {
-            if (command.equals("exit")) break;
-            try {
-                String result = handleCommand(command);
-                System.out.println(result);
-                response.println(result);
-            } catch (Exception e) {
-                response.println(e.getMessage());
+        try (
+            PrintWriter out = new PrintWriter(connection.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        ) {
+            System.out.println("Connected with " + connection.getInetAddress().getHostAddress());
+            out.println("Connected on " + System.getProperty("os.name") + ". Please type a CLI command for this OS, or exit to close connection." + "\n" + FINISH_MESSAGE);
+            String command;
+            while ((command = in.readLine()) != null) {
+                if (command.equals("exit")) break;
+                try {
+                    String result = handleCommand(command);
+                    System.out.println(result);
+                    out.println(result + "\n" + FINISH_MESSAGE);
+                } catch (Exception e) {
+                    out.println(e.getMessage());
+                }
             }
-        }
-        response.println("Connection closed");
+            out.println(CLOSED_MESSAGE);
 
-        reader.close();
-        response.close();
-        connection.close();
+            connection.close();
+        }
     }
 
     public static String handleCommand(String command) throws IOException {
         Process process = Runtime.getRuntime().exec(command);
-        StringBuilder response = new StringBuilder();
-
+        List<String> lines = new ArrayList<>();
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
         String line;
         while ((line = reader.readLine()) != null) {
-            response.append(line).append("\n");
+            System.out.println(line);
+            lines.add(line);
         }
 
-        process.getOutputStream();
         BufferedReader errorReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
         while ((line = errorReader.readLine()) != null) {
             System.out.println(line);
-            response.append(line);
+            lines.add(line);
         }
 
         errorReader.close();
         reader.close();
         process.destroy();
 
-        return response.toString();
+        return String.join("\n", lines);
     }
 }
