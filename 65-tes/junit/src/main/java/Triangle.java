cb106b7b14466203e2894e476b4dcbe63b764eb3
Mateus Lucas <mateuxlucax@gmail.com>
2022-08-30 19:54:04 -0300

    feat: add Triangle type

--- 65-tes/junit/src/main/java/Triangle.java
+++ 65-tes/junit/src/main/java/Triangle.java
@@ -1,13 +1,29 @@
 public class Triangle {
     private double height;
     private double base;
+    private double a;
+    private double b;
+    private double c;
+
     public Triangle() {
         this(0,0);
     }
+
     public Triangle(double base, double height) {
         setBase(base);
         setHeight(height);
+        setB(base);
+        double side = Math.sqrt((base / 2)) + Math.sqrt(height);
+        setA(side);
+        setB(side);
     }
+
+    public Triangle(double a, double b, double c)  {
+        setA(a);
+        setB(b);
+        setC(c);
+    }
+
     public double getHeight() {
         return height;
     }
@@ -26,5 +42,42 @@ public class Triangle {
 
     public double getArea() {
         return (getBase() * getHeight()) / 2;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+
+    public TriangleType getTriangleType() {
+        if (a == b && a == c) {
+            return TriangleType.EQUILATERAL;
+        } else if (a == b || a == c) {
+            return TriangleType.ISOSCELES;
+        }
+        return TriangleType.SCALENE;
+    }
+
+    public boolean sidesAreValid() {
+        return ((a < (b + c) && b < (a + c) && c < (a + b)));
     }
 }
--- 65-tes/junit/src/test/java/TriangleTest.java
+++ 65-tes/junit/src/test/java/TriangleTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class TriangleTest {
 
@@ -17,5 +18,35 @@ public class TriangleTest {
 
         // Assert
         assertEquals(expected, result, 0);
+    }
+
+    @Test
+    public void getTriangleType() {
+        // Arrange
+        double base = 2;
+        double height = 4;
+        TriangleType expected = TriangleType.ISOSCELES;
+
+        // Act
+        Triangle triangle = new Triangle(base, height);
+        TriangleType result = triangle.getTriangleType();
+
+        // Assert
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void sidesAreValid() {
+        // Arrange
+        double a = 2;
+        double b = 2;
+        double c = 2;
+
+        // Act
+        Triangle triangle = new Triangle(a, b, c);
+        boolean result = triangle.sidesAreValid();
+
+        // Assert
+        assertTrue(result);
     }
 }
